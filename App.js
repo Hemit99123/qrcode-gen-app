b515543ed290b4d3517b810b967d5826fce398a1
Hemit99123 <hemitpatel@computer4u.com>
2021-08-26 12:05:58 -0400

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useRef,  useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput
+} from 'react-native';
+import { captureRef } from 'react-native-view-shot';
+import Share from 'react-native-share';
+import QRCode from "react-qr-code";
 
 export default function App() {
+  const [text, setText] = useState('')
+  const viewRef = useRef();
+  const Shareable = async () => {
+try {
+  const uri = await captureRef(viewRef, {
+    format: 'png',
+    quality: 0.7
+  });
+  await Share.open({url: uri})
+} catch(err) {
+  console.error(err);
+}
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hemit's qr code generator</Text>
+      <TextInput 
+      style={styles.input}
+      placeholder='Enter text here'
+      onChangeText={(val) => setText(val)}
+      />
+      <Text>{text}</Text>
+      <TouchableOpacity onPress={Shareable} ref={viewRef}>
+      <QRCode value={text} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
